1e58b0aaada221bb6c9c9048aa8b928d992d942f
Alexandr Strukov <alxstrukov@yandex.ru>
2026-03-06 19:23:56 +0300

    Spring for Neil Alishev. Lesson 25

--- src/main/java/by/strukov/springcourse/model/Person.java
+++ src/main/java/by/strukov/springcourse/model/Person.java
@@ -24,20 +24,4 @@ public class Person {
 
     public Person() {
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
