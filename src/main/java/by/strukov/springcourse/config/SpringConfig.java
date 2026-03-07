8f9d0caa20f4558b1c61ac3345024ed1a60f9bdd
Alexandr Strukov <alxstrukov@yandex.ru>
2026-03-07 22:47:58 +0300

    Spring for Neil Alishev. Lessons 27-29

--- src/main/java/by/strukov/springcourse/config/SpringConfig.java
+++ src/main/java/by/strukov/springcourse/config/SpringConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,16 +16,28 @@ import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
-@Configuration
-@ComponentScan("by.strukov.springcourse")
+import javax.sql.DataSource;
+import java.util.Objects;
+
+@Configuration//помечает класс как конфигурационный
+@ComponentScan("by.strukov.springcourse")//указывает в каком пакете сканировать классы с бинами
 @EnableWebMvc
+/*@EnableWebMvc - эта аннотация включает инфраструктуру Spring MVC.
+Регистрируются обработчики запросов (HandlerMappings).
+Подключаются конвертеры и форматеры (например, преобразование String в Date).
+Включается поддержка @Controller, @RestController, @RequestBody и т.д.
+Настраивается сериализация JSON (через HttpMessageConverters).*/
+@PropertySource("classpath:database.properties")//указывает из какого файла .properties считывать данные
 public class SpringConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
+    private final Environment environment;
+    //environment - объект этого класса предоставляет методы для чтения данных из файла .properties
 
     @Autowired
-    public SpringConfig(ApplicationContext applicationContext) {
+    public SpringConfig(ApplicationContext applicationContext, Environment environment) {
         this.applicationContext = applicationContext;
+        this.environment = environment;
     }
 
     @Bean
@@ -47,5 +63,28 @@ public class SpringConfig implements WebMvcConfigurer {
         resolver.setTemplateEngine(templateEngine());
         resolver.setCharacterEncoding("UTF-8"); // Важно!
         registry.viewResolver(resolver);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName(environment.getProperty("driver"));
+//environment.getProperty("driver") - если драйвер не будет найден, то приложение не будет работать
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("spring.datasource.driver-class-name")));
+        //(Objects.requireNonNull(environment.getProperty("driver"))); - если драйвер не будет найден,
+        // то кинет исключение NullPointerException
+        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+        dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+/*        Важный момент: в файле database.properties название ключей обязательно нужно
+            начинать с spring.datasource., иначе Spring не считывает данные из файла,
+            либо переименовать файл в application.properties*/
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
     }
 }
--- src/main/java/by/strukov/springcourse/dao/PersonDAO.java
+++ src/main/java/by/strukov/springcourse/dao/PersonDAO.java
@@ -2,6 +2,9 @@ package by.strukov.springcourse.dao;
 
 
 import by.strukov.springcourse.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.sql.*;
@@ -10,103 +13,41 @@ import java.util.List;
 
 @Component
 public class PersonDAO {
-    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
-    private static Connection connection;
+    private final JdbcTemplate jdbcTemplate;
 
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<Person> index() {
-        List<Person> people = new ArrayList<>();
+        String sql = "SELECT * FROM person";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Person.class));
 
-        try {
-            Statement statement = connection.createStatement();
-            String query = "select * from person";
-            ResultSet resultSet = statement.executeQuery(query);
 
-            while (resultSet.next()) {
-                Person person = new Person();
-                person.setId(resultSet.getInt("id"));
-                person.setAge(resultSet.getInt("age"));
-                person.setName(resultSet.getString("name"));
-                person.setEmail(resultSet.getString("email"));
-                people.add(person);
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return people;
     }
 
     public Person show(int id) {
-        Person person = null;
-        try {
-            String query = "select * from person where id = ?";
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement(query);
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                person = new Person();
-                person.setId(resultSet.getInt("id"));
-                person.setAge(resultSet.getInt("age"));
-                person.setName(resultSet.getString("name"));
-                person.setEmail(resultSet.getString("email"));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return person;
+        String sql = "SELECT * FROM person WHERE id = ?";
+        List<Person> personList = jdbcTemplate.query(sql,
+                new Object[]{id}, new BeanPropertyRowMapper<>(Person.class));
+        return personList.stream().findAny().orElse(null);
     }
 
     public void save(Person person) {
-        try {
-            String query = "INSERT INTO person (id, name, age, email) VALUES (1,?,?,?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, person.getName());
-            preparedStatement.setInt(2, person.getAge());
-            preparedStatement.setString(3, person.getEmail());
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        String sql = "INSERT INTO person VALUES (1,?,?,?)";
+        jdbcTemplate.update(sql, person.getName(), person.getAge(), person.getEmail());
     }
 
-    public void update(int id, Person personUpdated) {
-        String query =
-                "UPDATE person set id = ?, name = ?, age = ?, email = ? where id = ?";
+    public void update(int id, Person updatedPerson) {
+        String sql = "UPDATE person SET name = ?, age = ?, email = ? where id = ?";
+        jdbcTemplate.update(sql, updatedPerson.getName(),
+                updatedPerson.getAge(), updatedPerson.getEmail(), id);
 
-        try {
-            PreparedStatement ps = connection.prepareStatement(query);
-            ps.setInt(1, id);
-            ps.setString(2, personUpdated.getName());
-            ps.setInt(3, personUpdated.getAge());
-            ps.setString(4, personUpdated.getEmail());
-            ps.setInt(5, id);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public void delete(int id) {
-        try {
-            String query = "delete from person where id = ?";
-            PreparedStatement ps = connection.prepareStatement(query);
-            ps.setInt(1, id);
-            ps.executeUpdate();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        String sql = "DELETE FROM person WHERE id = ?";
+        jdbcTemplate.update(sql, id);
     }
 }
